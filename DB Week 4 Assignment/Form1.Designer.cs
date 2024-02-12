252822602a91d0dbccdccc551d657643df2108f5
Oliver Crawford <oicrawford@gmail.com>
2024-02-11 20:19:30 -0600

    Finished coding assignment, can't test till in class.

--- DB Week 4 Assignment/Form1.Designer.cs
+++ DB Week 4 Assignment/Form1.Designer.cs
@@ -29,11 +29,20 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Input = new System.Windows.Forms.TextBox();
-            this.btnCreate = new System.Windows.Forms.Button();
-            this.btnRead = new System.Windows.Forms.Button();
-            this.btnUpdate = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
+            this.ReadOnlyID = new System.Windows.Forms.TextBox();
+            this.ReadOnlyFirstName = new System.Windows.Forms.TextBox();
+            this.ReadOnlyLastName = new System.Windows.Forms.TextBox();
+            this.ReadOnlyDOB = new System.Windows.Forms.TextBox();
+            this.ReadOnlyPhoneAllowed = new System.Windows.Forms.TextBox();
+            this.InputID = new System.Windows.Forms.TextBox();
+            this.inputFirstName = new System.Windows.Forms.TextBox();
+            this.InputLastName = new System.Windows.Forms.TextBox();
+            this.InputDOB = new System.Windows.Forms.TextBox();
+            this.InputAcceptsCalls = new System.Windows.Forms.RadioButton();
+            this.BtnRead = new System.Windows.Forms.Button();
+            this.BtnCreate = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,60 +53,163 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 150);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // Input
+            // ReadOnlyID
             // 
-            this.Input.Location = new System.Drawing.Point(12, 168);
-            this.Input.Name = "Input";
-            this.Input.Size = new System.Drawing.Size(776, 20);
-            this.Input.TabIndex = 1;
+            this.ReadOnlyID.Location = new System.Drawing.Point(12, 168);
+            this.ReadOnlyID.Name = "ReadOnlyID";
+            this.ReadOnlyID.ReadOnly = true;
+            this.ReadOnlyID.Size = new System.Drawing.Size(100, 20);
+            this.ReadOnlyID.TabIndex = 1;
+            this.ReadOnlyID.TabStop = false;
+            this.ReadOnlyID.Text = "ID";
+            this.ReadOnlyID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // btnCreate
+            // ReadOnlyFirstName
             // 
-            this.btnCreate.Location = new System.Drawing.Point(12, 300);
-            this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(75, 23);
-            this.btnCreate.TabIndex = 2;
-            this.btnCreate.Text = "Create";
-            this.btnCreate.UseVisualStyleBackColor = true;
+            this.ReadOnlyFirstName.Location = new System.Drawing.Point(12, 195);
+            this.ReadOnlyFirstName.Name = "ReadOnlyFirstName";
+            this.ReadOnlyFirstName.ReadOnly = true;
+            this.ReadOnlyFirstName.Size = new System.Drawing.Size(100, 20);
+            this.ReadOnlyFirstName.TabIndex = 2;
+            this.ReadOnlyFirstName.TabStop = false;
+            this.ReadOnlyFirstName.Text = "First Name";
+            this.ReadOnlyFirstName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // btnRead
+            // ReadOnlyLastName
             // 
-            this.btnRead.Location = new System.Drawing.Point(93, 300);
-            this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(75, 23);
-            this.btnRead.TabIndex = 3;
-            this.btnRead.Text = "Read";
-            this.btnRead.UseVisualStyleBackColor = true;
+            this.ReadOnlyLastName.Location = new System.Drawing.Point(12, 221);
+            this.ReadOnlyLastName.Name = "ReadOnlyLastName";
+            this.ReadOnlyLastName.ReadOnly = true;
+            this.ReadOnlyLastName.Size = new System.Drawing.Size(100, 20);
+            this.ReadOnlyLastName.TabIndex = 3;
+            this.ReadOnlyLastName.TabStop = false;
+            this.ReadOnlyLastName.Text = "Last Name";
+            this.ReadOnlyLastName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // btnUpdate
+            // ReadOnlyDOB
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(174, 300);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnUpdate.TabIndex = 4;
-            this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.ReadOnlyDOB.Location = new System.Drawing.Point(12, 247);
+            this.ReadOnlyDOB.Name = "ReadOnlyDOB";
+            this.ReadOnlyDOB.ReadOnly = true;
+            this.ReadOnlyDOB.Size = new System.Drawing.Size(100, 20);
+            this.ReadOnlyDOB.TabIndex = 4;
+            this.ReadOnlyDOB.TabStop = false;
+            this.ReadOnlyDOB.Text = "DOB";
+            this.ReadOnlyDOB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // btnDelete
+            // ReadOnlyPhoneAllowed
             // 
-            this.btnDelete.Location = new System.Drawing.Point(255, 300);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
-            this.btnDelete.TabIndex = 5;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.ReadOnlyPhoneAllowed.Location = new System.Drawing.Point(12, 273);
+            this.ReadOnlyPhoneAllowed.Name = "ReadOnlyPhoneAllowed";
+            this.ReadOnlyPhoneAllowed.ReadOnly = true;
+            this.ReadOnlyPhoneAllowed.Size = new System.Drawing.Size(100, 20);
+            this.ReadOnlyPhoneAllowed.TabIndex = 5;
+            this.ReadOnlyPhoneAllowed.TabStop = false;
+            this.ReadOnlyPhoneAllowed.Text = "Accepts calls";
+            this.ReadOnlyPhoneAllowed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // InputID
+            // 
+            this.InputID.Location = new System.Drawing.Point(119, 169);
+            this.InputID.Name = "InputID";
+            this.InputID.Size = new System.Drawing.Size(200, 20);
+            this.InputID.TabIndex = 7;
+            this.InputID.TextChanged += new System.EventHandler(this.InputID_TextChanged);
+            // 
+            // inputFirstName
+            // 
+            this.inputFirstName.Location = new System.Drawing.Point(119, 195);
+            this.inputFirstName.Name = "inputFirstName";
+            this.inputFirstName.Size = new System.Drawing.Size(200, 20);
+            this.inputFirstName.TabIndex = 8;
+            this.inputFirstName.TextChanged += new System.EventHandler(this.inputFirstName_TextChanged);
+            // 
+            // InputLastName
+            // 
+            this.InputLastName.Location = new System.Drawing.Point(118, 221);
+            this.InputLastName.Name = "InputLastName";
+            this.InputLastName.Size = new System.Drawing.Size(200, 20);
+            this.InputLastName.TabIndex = 9;
+            this.InputLastName.TextChanged += new System.EventHandler(this.InputLastName_TextChanged);
+            // 
+            // InputDOB
+            // 
+            this.InputDOB.Location = new System.Drawing.Point(118, 247);
+            this.InputDOB.Name = "InputDOB";
+            this.InputDOB.Size = new System.Drawing.Size(200, 20);
+            this.InputDOB.TabIndex = 10;
+            // 
+            // InputAcceptsCalls
+            // 
+            this.InputAcceptsCalls.AutoSize = true;
+            this.InputAcceptsCalls.Location = new System.Drawing.Point(118, 276);
+            this.InputAcceptsCalls.Name = "InputAcceptsCalls";
+            this.InputAcceptsCalls.Size = new System.Drawing.Size(14, 13);
+            this.InputAcceptsCalls.TabIndex = 11;
+            this.InputAcceptsCalls.TabStop = true;
+            this.InputAcceptsCalls.UseVisualStyleBackColor = true;
+            // 
+            // BtnRead
+            // 
+            this.BtnRead.Location = new System.Drawing.Point(348, 197);
+            this.BtnRead.Name = "BtnRead";
+            this.BtnRead.Size = new System.Drawing.Size(75, 23);
+            this.BtnRead.TabIndex = 12;
+            this.BtnRead.Text = "Read";
+            this.BtnRead.UseVisualStyleBackColor = true;
+            this.BtnRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnCreate
+            // 
+            this.BtnCreate.Location = new System.Drawing.Point(348, 168);
+            this.BtnCreate.Name = "BtnCreate";
+            this.BtnCreate.Size = new System.Drawing.Size(75, 23);
+            this.BtnCreate.TabIndex = 13;
+            this.BtnCreate.Text = "Create";
+            this.BtnCreate.UseVisualStyleBackColor = true;
+            this.BtnCreate.Click += new System.EventHandler(this.BtnCreate_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(348, 226);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.BtnUpdate.TabIndex = 14;
+            this.BtnUpdate.Text = "Update";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(348, 255);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 15;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.btnRead);
-            this.Controls.Add(this.btnCreate);
-            this.Controls.Add(this.Input);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnCreate);
+            this.Controls.Add(this.BtnRead);
+            this.Controls.Add(this.InputAcceptsCalls);
+            this.Controls.Add(this.InputDOB);
+            this.Controls.Add(this.InputLastName);
+            this.Controls.Add(this.inputFirstName);
+            this.Controls.Add(this.InputID);
+            this.Controls.Add(this.ReadOnlyPhoneAllowed);
+            this.Controls.Add(this.ReadOnlyDOB);
+            this.Controls.Add(this.ReadOnlyLastName);
+            this.Controls.Add(this.ReadOnlyFirstName);
+            this.Controls.Add(this.ReadOnlyID);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -111,11 +223,20 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox Input;
-        private System.Windows.Forms.Button btnCreate;
-        private System.Windows.Forms.Button btnRead;
-        private System.Windows.Forms.Button btnUpdate;
-        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox ReadOnlyID;
+        private System.Windows.Forms.TextBox ReadOnlyFirstName;
+        private System.Windows.Forms.TextBox ReadOnlyLastName;
+        private System.Windows.Forms.TextBox ReadOnlyDOB;
+        private System.Windows.Forms.TextBox ReadOnlyPhoneAllowed;
+        private System.Windows.Forms.TextBox InputID;
+        private System.Windows.Forms.TextBox inputFirstName;
+        private System.Windows.Forms.TextBox InputLastName;
+        private System.Windows.Forms.TextBox InputDOB;
+        private System.Windows.Forms.RadioButton InputAcceptsCalls;
+        private System.Windows.Forms.Button BtnRead;
+        private System.Windows.Forms.Button BtnCreate;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
     }
 }
 
--- DB Week 4 Assignment/Form1.cs
+++ DB Week 4 Assignment/Form1.cs
@@ -14,19 +14,117 @@ namespace DB_Week_4_Assignment
 {
     public partial class Form1 : Form
     {
+        //enter this when DB name available
+        string dbName = "";
         public Form1()
         {
             InitializeComponent();
             
         }
-        public void CreateTable()
-        {
 
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void InputID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputLastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnectionString = "server=localhost;uid=root;" + $"pwd=;database={dbName}";
+                MySqlConnection connection = new MySqlConnection(myConnectionString);
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("Select * from Contacts;", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                dataGridView1.DataSource = dataSet.Tables[0].DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnectionString = "server=localhost;uid=root;" + $"pwd=;database={dbName}";
+                MySqlConnection connection = new MySqlConnection(myConnectionString);
+                string query = $"insert into Contacts(ContactId, FirstName, LastName, DateOfBirth, AllowContactByPhone, createdDate) values({InputID.Text}, \'{inputFirstName.Text}\', \'{InputLastName.Text}\', \'{InputDOB.Text}\', {InputAcceptsCalls.Checked}, CURRENT_DATE());";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) { }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnectionString = "server=localhost;uid=root;" + $"pwd=;database={dbName}";
+                MySqlConnection connection = new MySqlConnection(myConnectionString);
+                string query = $"Update Contacts set FirstName=/'{inputFirstName.Text}/', LastName=/'{InputLastName.Text}/', DateOfBirth=/'{InputDOB.Text}/', AllowContactByPhone={InputAcceptsCalls.Checked} where ContactId={InputID.Text};";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) { }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnectionString = "server=localhost;uid=root;" + $"pwd=;database={dbName}";
+                MySqlConnection connection = new MySqlConnection(myConnectionString);
+                string query = $"delete from Contacts where ContactId={InputID.Text};";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) { }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
