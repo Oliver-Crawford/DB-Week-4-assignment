b23afd5c9c7ec13b50fd0bf57f806580367c10ae
Oliver Crawford <oicrawford@gmail.com>
2024-02-11 18:07:31 -0600

    Add project files.

---
+++ DB Week 4 Assignment/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DB_Week_4_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public void CreateTable()
+        {
+
+        }
+    }
+}
